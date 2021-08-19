6d13838ae41b1221d4cb05bac991c993fabbf2e8
fransHalason <franshalasonn@gmail.com>
2021-08-19 23:21:00 +0700

    introduction to data science with r

---
+++ R/1.1 Introduction to Data Science with R/Introduction to Data Science with R.R
@@ -0,0 +1,45 @@
+# Dasar-dasar Pemograman R
+
+# "Hello World"
+
+
+"Hello World"
+
+
+# Praktek Angka dan Perhitungan
+
+
+10 + 7
+
+
+# Variable dalam R
+
+
+a = 5
+print(a)
+
+
+# Lanjutan dasar-dasar R
+
+# Praktik Penggunaan Comment
+
+
+10 + 7 # Ini adalah baris komentar
+
+
+# Praktek Penggunaan Vector - Bagian Satu
+
+
+c(3, 10, 15)
+
+
+# Praktek Penggunaan Vector - Bagian Dua
+
+
+c(1:5)
+
+
+# Menggunakan Fungsi Summary
+
+
+summary(c(1:5))
